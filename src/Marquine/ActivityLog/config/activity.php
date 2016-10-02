a063520cb21e7ad54068180a27f4d0b2d3050433
Leonardo Marquine <leonardomarquine@gmail.com>
2016-10-02 16:47:39 -0300

    Add comments to config

--- src/Marquine/ActivityLog/config/activity.php
+++ src/Marquine/ActivityLog/config/activity.php
@@ -2,12 +2,44 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Activity Model
+    |--------------------------------------------------------------------------
+    |
+    | The model for the activities table.
+    | Needs to use the \Marquine\ActivityLog\ActivityLog Trait.
+    */
+
     'model' => App\Activity::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Activity Diff
+    |--------------------------------------------------------------------------
+    |
+    | To show the activity, we will calculate the difference between the state
+    | of the model before and after the activity. A Raw output will display
+    | data as it was saved, and will not go through mutators, casts, etc.
+    | The granularity determines the smallest text portion to compare.
+    |
+    | Supported granularities: "character", "word", "sentence", "paragraph"
+    |
+    */
 
     'diff' => [
         'raw' => true,
         'granularity' => 'word'
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Activity Log
+    |--------------------------------------------------------------------------
+    |
+    | Next, you can define the log options. The attributes in the except
+    | option will not be logged in the activities of a loggable model.
+    */
 
     'log' => [
         'except' => [
